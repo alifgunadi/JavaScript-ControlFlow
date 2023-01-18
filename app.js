a43549903f3ef205adb28f97a761498013e0ffec
Alif Gunadi <74938027+alifgunadi@users.noreply.github.com>
2023-01-18 21:39:59 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,311 @@
+let nilai = 100;
+let menu = prompt(`Input nilai:`);
+
+switch (menu) {
+    case "100":
+        alert(`Nilai anda A`)
+        break;
+    case "99":
+        alert(`Nilai anda A`)
+        break;
+    case "98":
+        alert(`Nilai anda A`)
+        break;
+    case "97":
+        alert(`Nilai anda A`)
+        break;
+    case "96":
+        alert(`Nilai anda A`)
+        break;
+    case "95":
+        alert(`Nilai anda A`)
+        break;
+    case "94":
+        alert(`Nilai anda A`)
+        break;
+    case "93":
+        alert(`Nilai anda A`)
+        break;
+    case "92":
+        alert(`Nilai anda A`)
+        break;
+    case "91":
+        alert(`Nilai anda A`)
+        break;
+    case "90":
+        alert(`Nilai anda A`)
+        break;
+    case "89":
+        alert(`Nilai anda A`)
+        break;
+    case "88":
+        alert(`Nilai anda A`)
+        break;
+    case "87":
+        alert(`Nilai anda A`)
+        break;
+    case "86":
+        alert(`Nilai anda A`)
+        break;
+    case "85":
+        alert(`Nilai anda A`)
+        break;
+    case "84":
+        alert(`Nilai anda A`)
+        break;
+    case "83":
+        alert(`Nilai anda A`)
+        break;
+    case "82":
+        alert(`Nilai anda A`)
+        break;
+    case "81":
+        alert(`Nilai anda A`)
+        break;
+    case "80":
+        alert(`Nilai anda A`)
+        break;
+    case "79":
+        alert(`Nilai anda B`)
+        break;
+    case "78":
+        alert(`Nilai anda B`)
+        break;
+    case "77":
+        alert(`Nilai anda B`)
+        break;
+    case "76":
+        alert(`Nilai anda B`)
+        break;
+    case "75":
+        alert(`Nilai anda B`)
+        break;
+    case "74":
+        alert(`Nilai anda B`)
+        break;
+    case "73":
+        alert(`Nilai anda B`)
+        break;
+    case "72":
+        alert(`Nilai anda B`)
+        break;
+    case "71":
+        alert(`Nilai anda B`)
+        break;
+    case "70":
+        alert(`Nilai anda B`)
+        break;
+    case "69":
+        alert(`Nilai anda B`)
+        break;
+    case "68":
+        alert(`Nilai anda B`)
+        break;
+    case "67":
+        alert(`Nilai anda B`)
+        break;
+    case "66":
+        alert(`Nilai anda B`)
+        break;
+    case "65":
+        alert(`Nilai anda B`)
+        break;
+    case "64":
+        alert(`Nilai anda B`)
+        break;
+    case "63":
+        alert(`Nilai anda B`)
+        break;
+    case "62":
+        alert(`Nilai anda B`)
+        break;
+    case "61":
+        alert(`Nilai anda B`)
+        break;
+    case "60":
+        alert(`Nilai anda B`)
+        break;
+    case "59":
+        alert(`Nilai anda C`)
+        break;
+    case "58":
+        alert(`Nilai anda C`)
+        break;
+    case "57":
+        alert(`Nilai anda C`)
+        break;
+    case "56":
+        alert(`Nilai anda C`)
+        break;
+    case "55":
+        alert(`Nilai anda C`)
+        break;
+    case "54":
+        alert(`Nilai anda C`)
+        break;
+    case "53":
+        alert(`Nilai anda C`)
+        break;
+    case "52":
+        alert(`Nilai anda C`)
+        break;
+    case "51":
+        alert(`Nilai anda C`)
+        break;
+    case "50":
+        alert(`Nilai anda C`)
+        break;
+    case "49":
+        alert(`Nilai anda C`)
+        break;
+    case "48":
+        alert(`Nilai anda C`)
+        break;
+    case "47":
+        alert(`Nilai anda C`)
+        break;
+    case "46":
+        alert(`Nilai anda C`)
+        break;
+    case "45":
+        alert(`Nilai anda C`)
+        break;
+    case "44":
+        alert(`Nilai anda C`)
+        break;
+    case "43":
+        alert(`Nilai anda C`)
+        break;
+    case "42":
+        alert(`Nilai anda C`)
+        break;
+    case "41":
+        alert(`Nilai anda C`)
+        break;
+    case "40":
+        alert(`Nilai anda C`)
+        break;
+    case "39":
+        alert(`Nilai anda D`)
+        break;
+    case "38":
+        alert(`Nilai anda D`)
+        break;
+    case "37":
+        alert(`Nilai anda D`)
+        break;
+    case "36":
+        alert(`Nilai anda D`)
+        break;
+    case "35":
+        alert(`Nilai anda D`)
+        break;
+    case "34":
+        alert(`Nilai anda D`)
+        break;
+    case "33":
+        alert(`Nilai anda D`)
+        break;
+    case "32":
+        alert(`Nilai anda D`)
+        break;
+    case "31":
+        alert(`Nilai anda D`)
+        break;
+    case "30":
+        alert(`Nilai anda D`)
+        break;
+    case "29":
+        alert(`Nilai anda D`)
+        break;
+    case "28":
+        alert(`Nilai anda D`)
+        break;
+    case "27":
+        alert(`Nilai anda D`)
+        break;
+    case "26":
+        alert(`Nilai anda D`)
+        break;
+    case "25":
+        alert(`Nilai anda D`)
+        break;
+    case "24":
+        alert(`Nilai anda D`)
+        break;
+    case "23":
+        alert(`Nilai anda D`)
+        break;
+    case "22":
+        alert(`Nilai anda D`)
+        break;
+    case "21":
+        alert(`Nilai anda D`)
+        break;
+    case "20":
+        alert(`Nilai anda D`)
+        break;
+    case "19":
+        alert(`Nilai anda E`)
+        break;
+    case "18":
+        alert(`Nilai anda E`)
+        break;
+    case "17":
+        alert(`Nilai anda E`)
+        break;
+    case "16":
+        alert(`Nilai anda E`)
+        break;
+    case "15":
+        alert(`Nilai anda E`)
+        break;
+    case "14":
+        alert(`Nilai anda E`)
+        break;
+    case "13":
+        alert(`Nilai anda E`)
+        break;
+    case "12":
+        alert(`Nilai anda E`)
+        break;
+    case "11":
+        alert(`Nilai anda E`)
+        break;
+    case "10":
+        alert(`Nilai anda E`)
+        break;
+    case "9":
+        alert(`Nilai anda E`)
+        break;
+    case "8":
+        alert(`Nilai anda E`)
+        break;
+    case "7":
+        alert(`Nilai anda E`)
+        break;
+    case "6":
+        alert(`Nilai anda E`)
+        break;
+    case "5":
+        alert(`Nilai anda E`)
+        break;
+    case "4":
+        alert(`Nilai anda E`)
+        break;
+    case "3":
+        alert(`Nilai anda E`)
+        break;
+    case "2":
+        alert(`Nilai anda E`)
+        break;
+    case "1":
+        alert(`Nilai anda E`)
+        break;
+    case "0":
+        alert(`Nilai anda E`)
+        break;
+    default:
+        alert(`Anda tidak menginput nilai maka, nilai anda E`)
+        break;
+}
